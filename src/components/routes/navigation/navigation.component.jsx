6a3f8960d9be0870e3108778b5e25b41d3a8ba87
BexRyd <becka_91_@hotmail.com>
2022-10-31 17:33:01 +0100

    logo

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Fragment } from "react"
+import {ReactComponent as CrwnLogo  } from "../../../assets/crown.svg"
+import { Outlet, Link } from "react-router-dom"
+import "./navigation.styles.scss"
+
 const Navigation = () =>{
     return(
-        <div>
-            <div>
-                <h1>I am the Navigation bar</h1>
+        <Fragment>
+            <div className="navigation">
+            <Link className="logo-container" to="/">
+              <CrwnLogo  className="logo"/>
+              </Link>
+              <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                Shop
+            </Link>
+              </div>
             </div>
             <Outlet/>
-        </div>
+        </Fragment>
     )
 }
 
